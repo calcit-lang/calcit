c747cce3db3cdf151c52ea2d025faec645237148
jiyinyiyong <jiyinyiyong@gmail.com>
2021-07-08 19:07:10 +0800

    temporary work on quoted EDN in calcit

--- src/data/edn.rs
+++ src/data/edn.rs
@@ -45,6 +45,23 @@ pub fn calcit_to_edn(x: &Calcit) -> Edn {
     Calcit::Fn(name, ..) => Edn::Str(format!("&fn {}", name)),
     Calcit::Proc(name) => Edn::Symbol(name.to_owned()),
     Calcit::Syntax(name, _ns) => Edn::Symbol(name.to_owned()),
+    Calcit::Tuple(tag, data) => {
+      match &**tag {
+        Calcit::Symbol(sym, ..) => {
+          if sym == "quote" {
+            match cirru::calcit_data_to_cirru(&**data) {
+              Ok(v) => Edn::Quote(v),
+              Err(e) => Edn::Str(format!("TODO quote {}", e)), // TODO more types to handle
+            }
+          } else {
+            Edn::Str(format!("TODO {}", sym)) // TODO more types to handle
+          }
+        }
+        v => {
+          Edn::Str(format!("TODO {}", v)) // TODO more types to handle
+        }
+      }
+    }
     a => Edn::Str(format!("TODO {}", a)), // TODO more types to handle
   }
 }
@@ -62,7 +79,15 @@ pub fn edn_to_calcit(x: &Edn) -> Calcit {
     ),
     Edn::Keyword(s) => Calcit::Keyword(s.clone()),
     Edn::Str(s) => Calcit::Str(s.clone()),
-    Edn::Quote(nodes) => cirru::cirru_to_calcit(nodes),
+    Edn::Quote(nodes) => Calcit::Tuple(
+      Box::new(Calcit::Symbol(
+        String::from("quote"),
+        String::from(primes::GENERATED_NS),
+        String::from(primes::GENERATED_DEF),
+        None,
+      )),
+      Box::new(cirru::cirru_to_calcit(nodes)),
+    ),
     Edn::List(xs) => {
       let mut ys: primes::CalcitItems = im::vector![];
       for x in xs {
