2d0fc0b183e25c37537d8a1045c7503a3dc8d471
jiyinyiyong <jiyinyiyong@gmail.com>
2021-09-13 23:28:53 +0800

    tag 0.4.32

--- ts-src/calcit.procs.ts
+++ ts-src/calcit.procs.ts
@@ -1,5 +1,5 @@
 // CALCIT VERSION
-export const calcit_version = "0.4.31";
+export const calcit_version = "0.4.32";
 
 import { overwriteComparator, initTernaryTreeMap } from "@calcit/ternary-tree";
 import { parse } from "@cirru/parser.ts";
