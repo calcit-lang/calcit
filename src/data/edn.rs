093381a08910369a335c96f09206986ec6d930b6
jiyinyiyong <jiyinyiyong@gmail.com>
2021-10-02 17:00:46 +0800

    upgrade to use compact EDN

--- src/data/edn.rs
+++ src/data/edn.rs
@@ -36,11 +36,11 @@ pub fn calcit_to_edn(x: &Calcit) -> Result<Edn, String> {
       Ok(Edn::Map(ys))
     }
     Calcit::Record(name, fields, values) => {
-      let mut ys: Vec<Edn> = vec![];
-      for v in values {
-        ys.push(calcit_to_edn(v)?)
+      let mut entries: Vec<(String, Edn)> = vec![];
+      for idx in 0..fields.len() {
+        entries.push((fields[idx].to_owned(), calcit_to_edn(&values[idx])?));
       }
-      Ok(Edn::Record(name.to_owned(), fields.to_owned(), ys))
+      Ok(Edn::Record(name.to_owned(), entries))
     }
     Calcit::Fn(name, ..) => Err(format!("unable to generate EDN from function: {}", name)),
     Calcit::Proc(name) => Ok(Edn::Symbol(name.to_owned())),
@@ -115,12 +115,14 @@ pub fn edn_to_calcit(x: &Edn) -> Calcit {
       }
       Calcit::Map(ys)
     }
-    Edn::Record(name, fields, values) => {
-      let mut ys: Vec<Calcit> = vec![];
-      for v in values {
-        ys.push(edn_to_calcit(v));
+    Edn::Record(name, entries) => {
+      let mut fields: Vec<String> = vec![];
+      let mut values: Vec<Calcit> = vec![];
+      for v in entries {
+        fields.push(v.0.to_owned());
+        values.push(edn_to_calcit(&v.1));
       }
-      Calcit::Record(name.to_owned(), fields.to_owned(), ys)
+      Calcit::Record(name.to_owned(), fields, values)
     }
   }
 }
