705e66a549bb8e83439e7d54be90b71d32094446
jiyinyiyong <jiyinyiyong@gmail.com>
2021-10-27 14:00:33 +0800

    refine FFI design and request for API_VERSION; tag 0.5.0-a18

--- ts-src/calcit.procs.ts
+++ ts-src/calcit.procs.ts
@@ -1,5 +1,5 @@
 // CALCIT VERSION
-export const calcit_version = "0.5.0-a17";
+export const calcit_version = "0.5.0-a18";
 
 import { overwriteComparator, initTernaryTreeMap } from "@calcit/ternary-tree";
 import { parse, ICirruNode } from "@cirru/parser.ts";
