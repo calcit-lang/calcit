38bdf0ad058fd133bb2c0d11ca0c0778b4225bba
jiyinyiyong <jiyinyiyong@gmail.com>
2021-11-07 20:07:07 +0800

    some verbose code for js perf; release 0.5.0

--- ts-src/calcit.procs.ts
+++ ts-src/calcit.procs.ts
@@ -1,5 +1,5 @@
 // CALCIT VERSION
-export const calcit_version = "0.5.0-a28";
+export const calcit_version = "0.5.0";
 
 import { overwriteComparator, initTernaryTreeMap } from "@calcit/ternary-tree";
 import { parse, ICirruNode } from "@cirru/parser.ts";
