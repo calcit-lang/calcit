5e632a2d8c3425d8546ad101b3c9bf0119567808
jiyinyiyong <jiyinyiyong@gmail.com>
2021-12-11 15:28:50 +0800

    upgrade ternary-tree; tag 0.5.14

--- ts-src/calcit.procs.ts
+++ ts-src/calcit.procs.ts
@@ -1,5 +1,5 @@
 // CALCIT VERSION
-export const calcit_version = "0.5.13";
+export const calcit_version = "0.5.14";
 
 import { overwriteComparator, initTernaryTreeMap } from "@calcit/ternary-tree";
 import { parse, ICirruNode } from "@cirru/parser.ts";
