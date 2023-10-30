d88b6cc6564461ab1e25b649488cbd6485d3b673
tiye <jiyinyiyong@gmail.com>
2023-10-31 02:07:58 +0800

    refactor: simplify syntax_name definition

--- src/primes/syntax_name.rs
+++ src/primes/syntax_name.rs
@@ -1,8 +1,5 @@
-use std::cmp::Ordering;
-use std::cmp::Ordering::*;
-
 /// core syntax inside Calcit
-#[derive(Debug, Clone, PartialEq, EnumString, strum_macros::Display)]
+#[derive(Debug, Clone, PartialEq, EnumString, strum_macros::Display, PartialOrd, Eq, Ord)]
 pub enum CalcitSyntax {
   #[strum(serialize = "defn")]
   Defn,
@@ -47,100 +44,10 @@ pub enum CalcitSyntax {
 }
 
 use strum_macros::EnumString;
-use CalcitSyntax::*;
 
 impl CalcitSyntax {
   /// check is given name is a syntax name
   pub fn is_valid(s: &str) -> bool {
-    matches!(
-      s,
-      "defn"
-        | "defmacro"
-        | "if"
-        | "&let"
-        | "quote"
-        | "quasiquote"
-        | "gensym"
-        | "eval"
-        | "macroexpand"
-        | "macroexpand-1"
-        | "macroexpand-all"
-        | "try"
-        | "defatom"
-        | "reset!"
-        | "hint-fn"
-    )
-  }
-}
-
-impl Ord for CalcitSyntax {
-  fn cmp(&self, other: &Self) -> Ordering {
-    match (self, other) {
-      (Defn, Defn) => Equal,
-      (Defn, _) => Less,
-      (_, Defn) => Greater,
-
-      (Defmacro, Defmacro) => Equal,
-      (Defmacro, _) => Less,
-      (_, Defmacro) => Greater,
-
-      (If, If) => Equal,
-      (If, _) => Less,
-      (_, If) => Greater,
-
-      (CoreLet, CoreLet) => Equal,
-      (CoreLet, _) => Less,
-      (_, CoreLet) => Greater,
-
-      (Quote, Quote) => Equal,
-      (Quote, _) => Less,
-      (_, Quote) => Greater,
-
-      (Quasiquote, Quasiquote) => Equal,
-      (Quasiquote, _) => Less,
-      (_, Quasiquote) => Greater,
-
-      (Gensym, Gensym) => Equal,
-      (Gensym, _) => Less,
-      (_, Gensym) => Greater,
-
-      (Eval, Eval) => Equal,
-      (Eval, _) => Less,
-      (_, Eval) => Greater,
-
-      (Macroexpand, Macroexpand) => Equal,
-      (Macroexpand, _) => Less,
-      (_, Macroexpand) => Greater,
-
-      (Macroexpand1, Macroexpand1) => Equal,
-      (Macroexpand1, _) => Less,
-      (_, Macroexpand1) => Greater,
-
-      (MacroexpandAll, MacroexpandAll) => Equal,
-      (MacroexpandAll, _) => Less,
-      (_, MacroexpandAll) => Greater,
-
-      (Try, Try) => Equal,
-      (Try, _) => Less,
-      (_, Try) => Greater,
-
-      (Defatom, Defatom) => Equal,
-      (Defatom, _) => Less,
-      (_, Defatom) => Greater,
-
-      (Reset, Reset) => Equal,
-      (Reset, _) => Less,
-      (_, Reset) => Greater,
-
-      (HintFn, HintFn) => Equal,
-    }
-  }
-}
-
-impl Eq for CalcitSyntax {}
-
-impl PartialOrd for CalcitSyntax {
-  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    Some(self.cmp(other))
+    s.parse::<CalcitSyntax>().is_ok()
   }
 }
